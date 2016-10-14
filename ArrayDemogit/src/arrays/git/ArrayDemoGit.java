63b1d79653fa5659648d2d06b325067535af4a4e
MD1709 <mduong1709@gmail.com>
2016-10-14 16:24:14 +0700

    Ubuntu commit

--- ArrayDemogit/src/arrays/git/ArrayDemoGit.java
+++ ArrayDemogit/src/arrays/git/ArrayDemoGit.java
@@ -2,13 +2,9 @@ package arrays.git;
 
 public class ArrayDemoGit {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		int[] arr = {1, 2, 3, 4, 9, 10, 11, 12};
 		for(int i : arr){
-=======
-		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-		for(int i = 0 ; i < arr.length ; i++){
->>>>>>> branch 'master' of https://github.com/MD1709/ArrayDemo.git
 			System.out.println(arr[i]);
 		}
 	}
