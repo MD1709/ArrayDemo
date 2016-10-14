f51dd3ab371b55898fa2ecd46253ac399d34a5b5
DuongMinh <duongmb95@gmail.com>
2016-10-14 16:08:50 +0700

    Window Commit

--- ArrayDemogit/src/arrays/git/ArrayDemoGit.java
+++ ArrayDemogit/src/arrays/git/ArrayDemoGit.java
@@ -2,8 +2,8 @@ package arrays.git;
 
 public class ArrayDemoGit {
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 3, 4};
-		for(int i = 0 ; i < arr.length ; i++){
+		int[] arr = {1, 2, 3, 4, 9, 10, 11, 12};
+		for(int i : arr){
 			System.out.println(arr[i]);
 		}
 	}
