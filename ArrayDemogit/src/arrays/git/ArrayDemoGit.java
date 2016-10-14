3e6c7151783352eb8d9db3a90b84ffcdcc9f3f1f
MD1709 <mduong1709@gmail.com>
2016-10-14 16:03:19 +0700

    Init commit

---
+++ ArrayDemogit/src/arrays/git/ArrayDemoGit.java
@@ -0,0 +1,10 @@
+package arrays.git;
+
+public class ArrayDemoGit {
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4};
+		for(int i = 0 ; i < arr.length ; i++){
+			System.out.println(arr[i]);
+		}
+	}
+}
